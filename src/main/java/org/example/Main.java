fd534ce9ee0736675cf38e1091a8709594aa1837
TenorioNicolas <n.tenorio01@ufromail.cl>
2024-04-21 14:21:35 -0400

    Ingreso de usuarios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] usuarios = {"123456789", "987654321"};
+        String[] contrasenas = {"contraseña1", "contraseña2"};
+
+        ingresarUsuario(usuarios, contrasenas);
+
+    }
+    public static void ingresarUsuario(String[] usuarios, String[] contrasenas) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el usuario: ");
+        String usuario = scanner.nextLine();
+        System.out.print("Ingrese la contraseña: ");
+        String contrasena = scanner.nextLine();
+
+        if (validarUsuario(usuario, contrasena, usuarios, contrasenas)) {
+            System.out.println("Usuario y contraseña válidos. Bienvenido.");
+        } else {
+            System.out.println("Usuario o contraseña incorrectos.");
+        }
+    }
+    public static boolean validarUsuario(String usuario, String contrasena, String[] usuarios, String[] contrasenas) {
+        for (int i = 0; i < usuarios.length; i++) {
+            if (usuarios[i].equals(usuario) && contrasenas[i].equals(contrasena)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
+
